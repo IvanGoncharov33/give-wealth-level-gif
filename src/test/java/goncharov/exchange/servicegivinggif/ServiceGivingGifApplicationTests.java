209c8a48f0b91eb237dd6ebe7441ca4c164cf741
Ivan Goncharov <Van4es7077@yandex.ru>
2021-02-20 20:53:49 +0300

    initial commit

---
+++ src/test/java/goncharov/exchange/servicegivinggif/ServiceGivingGifApplicationTests.java
@@ -0,0 +1,13 @@
+package goncharov.exchange.servicegivinggif;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServiceGivingGifApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
