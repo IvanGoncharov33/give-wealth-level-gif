054ff30e8fd950b5fcd525d35136843b27327c84
Ivan Goncharov <Van4es7077@yandex.ru>
2021-02-25 00:28:43 +0300

    added configurations

---
+++ src/main/java/goncharov/app/config/ExchangeRateFeignClientConfig.java
@@ -0,0 +1,20 @@
+package goncharov.app.config;
+
+import feign.RequestInterceptor;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpHeaders;
+
+@Configuration
+@IgnoreConfig
+public class ExchangeRateFeignClientConfig {
+
+    @Value("${exchange.appId}")
+    private String appId;
+
+    @Bean
+    public RequestInterceptor requestInterceptorExchangeRate(){
+        return template -> template.header(HttpHeaders.AUTHORIZATION, String.format("Token %s", appId));
+    }
+}
