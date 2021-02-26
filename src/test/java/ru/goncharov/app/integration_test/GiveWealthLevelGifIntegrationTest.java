d3162d5003cb4419dba91282dcf6b42703505148
Ivan Goncharov <Van4es7077@yandex.ru>
2021-02-26 14:56:54 +0300

    Added integration test

--- src/test/java/ru/goncharov/app/integration_test/GiveWealthLevelGifIntegrationTest.java
+++ src/test/java/ru/goncharov/app/integration_test/GiveWealthLevelGifIntegrationTest.java
@@ -8,8 +8,10 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.test.web.servlet.MockMvc;
 import ru.goncharov.app.controller.GifController;
 import ru.goncharov.app.wiremock.clientgif.GifClientMock;
 import ru.goncharov.app.wiremock.exchange.ExchangeRateClientMock;
@@ -19,9 +21,12 @@ import java.io.IOException;
 import java.util.Objects;
 
 import static org.junit.jupiter.api.Assertions.assertArrayEquals;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @EnableConfigurationProperties
 @ExtendWith(SpringExtension.class)
+@AutoConfigureMockMvc
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.DEFINED_PORT)
 class GiveWealthLevelGifIntegrationTest {
 
@@ -30,6 +35,8 @@ class GiveWealthLevelGifIntegrationTest {
     private WireMockServer mockGifClient;
     @Autowired
     private GifController gifController;
+    @Autowired
+    private MockMvc mockMvc;
 
     @Value("${exchange.currencyCode}")
     private String currencyCode;
@@ -57,15 +64,21 @@ class GiveWealthLevelGifIntegrationTest {
     }
 
     @Test
-    void shouldGetGifWealthLevelBroke() throws IOException {
+    void shouldGetGifWealthLevelBroke() throws Exception {
         ExchangeRateClientMock.setUpExchangeRateWealthLevelBrokeResponse(mockExchangeClient);
         assertArrayEquals(brokeGif, gifController.getGif(currencyCode).getBody());
     }
 
     @Test
-    void shouldGetGifWealthLevelRich() throws IOException {
+    void shouldGetGifWealthLevelRich() throws Exception {
         ExchangeRateClientMock.setUpExchangeRateWealthLevelRichResponse(mockExchangeClient);
         assertArrayEquals(richGif, gifController.getGif(currencyCode).getBody());
+    }
+
+    @Test
+    void gifControllerTest() throws Exception {
+        ExchangeRateClientMock.setUpExchangeRateWealthLevelRichResponse(mockExchangeClient);
+        mockMvc.perform(get("/api/gif/{currencyCode}",currencyCode)).andExpect(status().isOk());
     }
 
     @AfterEach
